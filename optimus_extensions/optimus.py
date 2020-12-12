eb8e7d86168772ab5e1df766b3d29cbae126c095
root <zorzac2001@gmail.com>
2020-12-12 22:21:35 +0000

    Added a notebook file and made it a module.

---
+++ optimus_extensions/optimus.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("boom")
+
+
+# In[2]:
+
+
+print("I expect respect")
+
+
+# In[3]:
+
+
+print("magnolia")
+
+
+# In[4]:
+
+
+print("pusy bitch")
+
+
+# In[5]:
+
+
+print("Optimus")
+
+
+# In[6]:
+
+
+print("Cyrus")
+
